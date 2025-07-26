e1a929f1a994403aa156b978be3c19482650964c
psychicDree <devmanjha@gmail.com>
2025-07-26 15:37:58 +0530

    Updates

--- _next/static/chunks/app/layout-1f4411e3d4e643c9.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{5356:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_e8ce0c"}},5786:()=>{},7468:(e,s,l)=>{Promise.resolve().then(l.t.bind(l,5356,23)),Promise.resolve().then(l.t.bind(l,5786,23))}},e=>{e.O(0,[283,441,964,358],()=>e(e.s=7468)),_N_E=e.O()}]);
